f8715b2e9d6af159d48dddc16ea654a80ecc45b0
Selmene <enemles.ihraf@gmail.com>
2025-03-12 22:49:30 +0100

    wip

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -423,11 +423,77 @@ export const updateUser = async (user: Partial<User>) => {
     data: { ...user },
   })
 
+  //TODO
   // await clerkClient.users.updateUserMetadata(response.id, {
   //   privateMetadata: {
   //     role: user.role || 'SUBACCOUNT_USER',
   //   },
   // })
+
+  return response
+}
+
+export const getSubaccountDetails = async (subaccountId: string) => {
+  const response = await db.subAccount.findUnique({
+    where: {
+      id: subaccountId,
+    },
+  })
+  return response
+}
+
+export const deleteSubAccount = async (subaccountId: string) => {
+  const response = await db.subAccount.delete({
+    where: {
+      id: subaccountId,
+    },
+  })
+  return response
+}
+
+export const deleteUser = async (userId: string) => {
+  await clerkClient.users.updateUserMetadata(userId, {
+    privateMetadata: {
+      role: undefined,
+    },
+  })
+  const deletedUser = await db.user.delete({ where: { id: userId } })
+
+  return deletedUser
+}
+
+export const getUser = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  return user
+}
+
+export const sendInvitation = async (
+  role: Role,
+  email: string,
+  agencyId: string
+) => {
+  const response = await db.invitation.create({
+    data: { email, agencyId, role },
+  })
+
+  try {
+    const invitation = await clerkClient.invitations.createInvitation({
+      emailAddress: email,
+      redirectUrl: process.env.NEXT_PUBLIC_URL,
+      publicMetadata: {
+        throughInvitation: true,
+        role,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 
   return response
 }
