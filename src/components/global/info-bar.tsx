a6ea767381ce879810fbacf378343fba700962b4
Selmene <enemles.ihraf@gmail.com>
2025-04-18 13:34:15 +0200

    🐛  fix info bar causing a crash

--- src/components/global/info-bar.tsx
+++ src/components/global/info-bar.tsx
@@ -24,11 +24,11 @@ function InfoBar({ notifications, role, className, subAccountId }: Props) {
   const handleClick = () => {
     if (!showAll) {
       setAllNotifications(notifications)
-
     } else {
       if (notifications?.length !== 0) {
         setAllNotifications(
-          notifications?.filter((item) => item.subAccountId === subAccountId) ?? []
+          notifications?.filter((item) => item.subAccountId === subAccountId) ??
+            []
         )
       }
     }
@@ -44,51 +44,53 @@ function InfoBar({ notifications, role, className, subAccountId }: Props) {
         )}
       >
         <div className="flex items-center gap-2 ml-auto">
-          <UserButton afterSignOutUrl='/' />
+          <UserButton afterSignOutUrl="/" />
           <Sheet>
             <SheetTrigger>
               <div className="rounded-full w-9 h-9 bg-primary flex items-center justify-center text-white">
                 <Bell size={17} />
               </div>
             </SheetTrigger>
-            <SheetContent className='mt-4 mr-4 pr-4 flex flex-col'>
-              <SheetHeader className='text-left'>
+            <SheetContent className="mt-4 mr-4 pr-4 overflow-scroll">
+              <SheetHeader className="text-left">
                 <SheetTitle>Notifications</SheetTitle>
                 <SheetDescription>
-                  {(role === 'AGENCY_ADMIN' || role === 'AGENCY_OWNER') &&
-                    <Card className='flex items-center justify-between p-4'>
+                  {(role === 'AGENCY_ADMIN' || role === 'AGENCY_OWNER') && (
+                    <Card className="flex items-center justify-between p-4">
                       Current Subaccount
-                      <Switch onChangeCapture={handleClick} />
+                      <Switch onCheckedChange={handleClick} />
                     </Card>
-                  }
+                  )}
                 </SheetDescription>
               </SheetHeader>
               {allNotifications?.map((notification) => (
-                <div key={notification.id} className='flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis'>
+                <div
+                  key={notification.id}
+                  className="flex flex-col gap-y-2 mb-2 overflow-x-scroll text-ellipsis"
+                >
                   <div className="flex gap-2">
                     <Avatar>
                       <AvatarImage
                         src={notification.User.avatarUrl}
-                        alt='Profile Picture'
-                      >
-                        <AvatarFallback className='bg-primary'>
-                          {notification.User.name.slice(0, 2).toUpperCase()}
-                        </AvatarFallback>
-                      </AvatarImage>
+                        alt="Profile Picture"
+                      />
+                      <AvatarFallback className="bg-primary">
+                        {notification.User.name.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
                     </Avatar>
-                    <div className='flex flex-col'>
+                    <div className="flex flex-col">
                       <p>
-                        <span className='font-bold'>
+                        <span className="font-bold">
                           {notification.notification.split('|')[0]}
                         </span>
-                        <span className='text-muted-foreground'>
+                        <span className="text-muted-foreground">
                           {notification.notification.split('|')[1]}
                         </span>
-                        <span className='font-bold'>
+                        <span className="font-bold">
                           {notification.notification.split('|')[2]}
                         </span>
                       </p>
-                      <small className='text-xs text-muted-foreground'>
+                      <small className="text-xs text-muted-foreground">
                         {new Date(notification.createdAt).toLocaleDateString()}
                       </small>
                     </div>
@@ -96,8 +98,11 @@ function InfoBar({ notifications, role, className, subAccountId }: Props) {
                 </div>
               ))}
               {allNotifications?.length === 0 && (
-                <div className='flex items-center justify-center mb-4'>
-                  You have no notifications !
+                <div
+                  className="flex items-center justify-center text-muted-foreground"
+                  mb-4
+                >
+                  You have no notifications
                 </div>
               )}
             </SheetContent>
