72f06bca6605e88499736d20a95fdd870db5d928
Selmene <enemles.ihraf@gmail.com>
2025-05-08 10:23:06 +0200

    add analytics to project

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/next';
 import { dark } from '@clerk/themes'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>{children}
+    <Analytics />
+    </ClerkProvider>
   )
 }
 
